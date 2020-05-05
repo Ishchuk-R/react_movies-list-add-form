614db629806184740619e46225083d7582d9a3e4
Tetiana <ishchuktr@gmail.com>
2020-05-05 23:01:37 +0300

    Updates

---
+++ precache-manifest.10d6bc6cade21c298dd2ad87f708d093.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22ff8f6649eaab963f759ba103e026a7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "966e43fcb559617e11a8",
+    "url": "./static/css/2.cd1899df.chunk.css"
+  },
+  {
+    "revision": "7e4167ec60301a3a06c5",
+    "url": "./static/css/main.41b8b0f1.chunk.css"
+  },
+  {
+    "revision": "966e43fcb559617e11a8",
+    "url": "./static/js/2.f5dfd04b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.f5dfd04b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e4167ec60301a3a06c5",
+    "url": "./static/js/main.fe1db5d1.chunk.js"
+  },
+  {
+    "revision": "93138561b2093b9a2516",
+    "url": "./static/js/runtime-main.8aef587d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2088c32903613710f8f9678a01b40cd4.js"
+  "./precache-manifest.10d6bc6cade21c298dd2ad87f708d093.js"
 );
 
 self.addEventListener('message', (event) => {
